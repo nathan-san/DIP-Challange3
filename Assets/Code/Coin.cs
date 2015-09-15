966cb2eaade944fc6f486be3e257797e915d8ebf
nathan-san <18964@ma-web.nl>
2015-09-15 11:49:04 +0200

    I added the result
    .

---
+++ Assets/Code/Coin.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : Pickup//inherit van pickup
+{
+    //override = je maakt een extensie van die functie
+    public override void PlayerHit(Player _GO)
+    {
+        base.PlayerHit(_GO);//roep de basis classe nog wel aan(dus de playerhit in pickup)
+		_GO.m_score ++;
+		Debug.Log (_GO.m_score);
+        //voeg hier code toe zo dat de speler zijn score omhoog gaat
+    }
+}
---
+++ Assets/Code/Player.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public float m_score = 0;
+	public float speed = 4;
+	public bool rot = false;
+    // Update is called once per frame
+    void Update()
+    {
+        //beweeg de speler naar rechts
+        this.transform.position += Vector3.right * Time.deltaTime * speed;
+		if (rot)
+		{
+			this.transform.Rotate (0f,0f,1f);
+		}
+    }
+}
